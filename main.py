d1dbdff3f0f7477b79deefc4f03bd6c0980d98af
Geoff Arbuckle <arbuckle.geoff@icloud.com>
2025-05-05 14:07:59 -0300

    Updated to Ch3 Lesson2

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
+import pygame
+import random
+from asteroid import Asteroid
+from constants import *
+
+
+class AsteroidField(pygame.sprite.Sprite):
+    edges = [
+        [
+            pygame.Vector2(1, 0),
+            lambda y: pygame.Vector2(-ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
+        ],
+        [
+            pygame.Vector2(-1, 0),
+            lambda y: pygame.Vector2(
+                SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT
+            ),
+        ],
+        [
+            pygame.Vector2(0, 1),
+            lambda x: pygame.Vector2(x * SCREEN_WIDTH, -ASTEROID_MAX_RADIUS),
+        ],
+        [
+            pygame.Vector2(0, -1),
+            lambda x: pygame.Vector2(
+                x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS
+            ),
+        ],
+    ]
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.spawn_timer = 0.0
+
+    def spawn(self, radius, position, velocity):
+        asteroid = Asteroid(position.x, position.y, radius)
+        asteroid.velocity = velocity
+
+    def update(self, dt):
+        self.spawn_timer += dt
+        if self.spawn_timer > ASTEROID_SPAWN_RATE:
+            self.spawn_timer = 0
+
+            # spawn a new asteroid at a random edge
+            edge = random.choice(self.edges)
+            speed = random.randint(40, 100)
+            velocity = edge[0] * speed
+            velocity = velocity.rotate(random.randint(-30, 30))
+            position = edge[1](random.uniform(0, 1))
+            kind = random.randint(1, ASTEROID_KINDS)
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
 
 def main():
     pygame.init()
@@ -15,17 +67,28 @@ def main():
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x,y)
+    asteroid_field = AsteroidField()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
